2641d5011bc4be0f3f81e110010af924927ccaeb
Diptesh Choudhuri <ignisda2002@gmail.com>
2022-11-28 14:55:19 +0530

    ci: change devcontainer setup
    Use archlinux containers for devcontainer.
    
    skip ci

--- .devcontainer/scripts/post-create.sh
+++ .devcontainer/scripts/post-create.sh
@@ -3,8 +3,3 @@
 # setup the git identities
 git config --global user.name "${GIT_AUTHOR_NAME}"
 git config --global user.email "${GIT_AUTHOR_EMAIL}"
-
-# to install cargo binaries
-cargo install cargo-binstall
-# install cargo test runner and file watcher
-cargo binstall cargo-watch cargo-nextes cargo-expand --secure --no-confirm
