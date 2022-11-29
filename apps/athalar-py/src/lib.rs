a78ffaef73e5ac39cf08287b519fc24a1cf11e42
Diptesh Choudhuri <ignisda2001@gmail.com>
2022-11-29 11:12:47 +0000

    feat: add initial project for python impl

---
+++ apps/athalar-py/src/lib.rs
@@ -0,0 +1,12 @@
+use pyo3::{pyfunction, pymodule, PyResult, wrap_pyfunction, Python, types::PyModule};
+
+#[pyfunction]
+fn sum_as_string(a: usize, b: usize) -> PyResult<String> {
+    Ok((a + b).to_string())
+}
+
+#[pymodule]
+fn athalar_py(_py: Python, m: &PyModule) -> PyResult<()> {
+    m.add_function(wrap_pyfunction!(sum_as_string, m)?)?;
+    Ok(())
+}
